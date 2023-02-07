a245c7b882f5a584e2a96c3ede1550c23f9c822a
Vinicius Gabriel Martins de Lima <vinicius.g@academico.ifrn.edu.br>
2023-02-07 16:53:47 -0300

    meio caminho andado

--- src/main/java/ifrn/pi/comercio/models/Usuario.java
+++ src/main/java/ifrn/pi/comercio/models/Usuario.java
@@ -1,25 +1,74 @@
 package ifrn.pi.comercio.models;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
 
 @Entity
-public class Usuario {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
-	private String email;
-	private String senha;
-	private String tipo;
+@Table(name = "TB_USER")
+public class Usuario implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
 	
-	public Long getId() {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	@Column(nullable = false, unique = true)
+	private String nome;
+	@Column(nullable = false)
+	private String senha;
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.senha;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.nome;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	public UUID getId() {
 		return id;
 	}
-	public void setId(Long id) {
+	public void setId(UUID id) {
 		this.id = id;
 	}
 	public String getNome() {
@@ -28,22 +77,16 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	public String getSenha() {
 		return senha;
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	public String getTipo() {
-		return tipo;
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}	
+	
+	
+	
 }
