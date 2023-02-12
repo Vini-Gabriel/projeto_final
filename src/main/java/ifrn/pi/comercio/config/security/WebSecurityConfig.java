4b63613f897ff7e15715f3063951bc1a27ff93ae
Vinicius Gabriel <vini.gabriel.m@gmail.com>
2023-02-11 22:52:50 -0300

    agora sim, tudo de acordo com o video

--- src/main/java/ifrn/pi/comercio/config/security/WebSecurityConfig.java
+++ src/main/java/ifrn/pi/comercio/config/security/WebSecurityConfig.java
@@ -2,34 +2,30 @@ package ifrn.pi.comercio.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
-	protected void configure(HttpSecurity http) throws Exception{
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http 
-			
-				.httpBasic()
-				.and()
-				.authorizeHttpRequests()
-				.anyRequest().authenticated()
-				.and()
-				.csrf().disable();
+			.httpBasic()
+			.and()
+			.authorizeHttpRequests()
+			.antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+			.antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+			.csrf().disable();
+		return http.build();
 	}
 	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		
-		auth.inMemoryAuthentication()
-		.withUser("ifrn")
-		.password(passwordEncoder().encode("123456"))
-		.roles("ADMIN");
-	}
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
